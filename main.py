3b14228b44afeacf66c647c25974b7ffb1c76f5e
Emrah ERTAN <75017522+emrah-ertan@users.noreply.github.com>
2022-12-25 23:21:29 +0300

    Contour function added
    Added find_contour() method.

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+def find_contour(image_open,image_original):
+  contours, _ = cv2.findContours(image_open,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+  cv2.drawContours(image_original,contours,-1,(0,255,0),2)
+  print("Görüntüde {} adet nesne belirlendi.".format(len(contours)))
+  cv2.imshow("Belirlenen Nesneler",image_original)
+
 img = cv2.imread("uzay.jpg")
 img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 ret, img_thresh = cv2.threshold(img_gray,90,255,cv2.THRESH_BINARY)
 cv2.imshow("Thresh img",img_thresh)
-
 
 kernel = np.ones((5,5),dtype=np.uint8)
 img_erode = cv2.erode(img_thresh,kernel,iterations=1)    # erozyon
@@ -25,5 +30,7 @@ cv2.imshow("Close img",img_closing)
 
 img_gradiant = img_dilate - img_erode
 cv2.imshow("Gradiant img",img_gradiant)
+
+find_contour(img_opening,img)
 
 cv2.waitKey(0)
